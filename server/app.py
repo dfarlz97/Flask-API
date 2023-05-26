ff00b6b4857f2caf7fc4b179c359ccb5b6c37cff
+ <+>
2023-05-26 13:29:50 -0400

    seed db

--- server/app.py
+++ server/app.py
@@ -1,28 +1,50 @@
-# step 1: install Flask Library 
-    # pipenv install 
-    # pipenv shell
-    # touch server
-    # cd server
-    # touch app.py
-    # pipenv install Flask (from main directory)
-
-#Step 2: import flask 
+# 1. install Flask library 
 from flask import Flask
+from flask_restful import Api, Resource, request
 
 from flask_migrate import Migrate
 
 from models import db, Student, Teacher, Review
 
-#Step 3: create flask application
+# 2. creating the flask application
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///app.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# migration --> init db 
 
 Migrate(app, db)
 
 db.init_app(app)
 
+
+api = Api(app)
+
+# student/teacher/review
+
 @app.route("/")
 def index():
     return {"message": "Hello World!"}
+
+class Students(Resource):
+    
+    def get(self):
+        students = [ {
+            "name": s.name, 
+            "phase": s.phase
+        } for s in Student.query.all() ]
+        
+        
+        return students, 200
+    
+    def post(self): 
+        student = Student(
+            name = request.json['name'],
+            phase = request.json['phase'],
+            email = request.json['email'],
+            superhero_partner = request.json['superhero_partner'])
+        
+        db.session.add(student)
+        db.session.commit()
+        
+        return {"name": student.name, "phase": student.phase}, 201
+    
+api.add_resource(Students, "/students")
